45b3ce3b974eeacd1d586dbd902ea3800d0e6206
Marcos <marcosfreytag@gmail.com>
2024-07-15 18:44:40 -0300

    Add health

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,8 @@
 from resources.user import User
 from resources.users import Users
+from resources.health import Health
 
 def initialize_routes(api):
+    api.add_resource(Health, '/health')
     api.add_resource(User, '/user', '/user/<string:username>')
     api.add_resource(Users, '/users')
