8558596a448002c845ef8cfb1af81458436eba8a
Marcos <marcosfreytag@gmail.com>
2024-07-15 23:20:52 -0300

    Ajustes var ecs

--- infrastructure/ecs/variables.tf
+++ infrastructure/ecs/variables.tf
@@ -32,13 +32,5 @@ variable "env_vars" {
 }
 
 variable "log_configuration" {
-  type = list(object({
-    logDriver = string
-    options = object({
-      awslogs-create-group = string
-      awslogs-region = string
-      awslogs-group = string
-      awslogs-stream-prefix = string
-    })
-  }))
+
 }
